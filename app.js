679e971fb3bf4fbfd687c60300d6e522615ff0f2
JustReddy <70798725+JustReddy7397@users.noreply.github.com>
2020-12-18 16:48:03 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+  
+require('dotenv').config();
+require('./strategies/discord');
+
+const express = require("express");
+const passport = require("passport")
+const chalk = require("chalk");
+const app = express();
+
+const PORT = process.env.PORT || 3910;
+
+const routes = require("./routes");
+
+app.use('/api', routes);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.listen(PORT, () => console.log(chalk.green("Listening to port " + chalk.blue(PORT))));
